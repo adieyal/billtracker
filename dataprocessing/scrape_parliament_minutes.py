a42a77af44687065c5a79d903c5b26f3abd9987d
Adi Eyal <adi@burgercom.co.za>
2013-07-09 20:14:34 +0200

    Moved scraping files into dataprocessing

---
+++ dataprocessing/scrape_parliament_minutes.py
@@ -0,0 +1,51 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+filedir = "files"
+site_url = "http://www.parliament.gov.za/live/"
+url = "http://www.parliament.gov.za/live/content.php?Category_ID=228&DocumentStart=%s"
+
+def download_file(url, name):
+    #print "Downloading: %s from %s" % (name, url)
+    if not os.path.exists(filedir):
+        os.mkdir(filedir)
+
+    path = os.path.join(filedir, name)
+    if os.path.exists(path):
+        print "Skipped: %s" % name
+        return
+
+    r = requests.get(url)
+    if r.status_code == 200:
+        with open(path, 'wb') as f:
+            for chunk in r.iter_content():
+                f.write(chunk)
+    else:
+        print "Could not download file: %s" % name
+
+def scrape():
+    for i in range(0, 200, 10):
+        r = requests.get(url % i)
+        
+        soup = BeautifulSoup(r.text)
+        table = soup.select(".tableOrange_sep")[0]
+        rows = table.select("tr")[3:100:4][0:10]
+        for row in rows:
+            cells = row.select("td")
+            filename = cells[0].text
+            date = cells[2].text 
+            house = cells[4].text
+            language = cells[6].text
+            file_url = os.path.join(site_url, cells[10].a["href"])
+            data = {
+                "filename" : filename,
+                "date" : date,
+                "house" : house,
+                "language" : language,
+                "url" : file_url,
+            }
+            download_file(file_url, filename + ".pdf")
+
+if __name__ == "__main__":
+    scrape()
