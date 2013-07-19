567b7ae8b924d2e87663a43d5be46eee15b4018d
Adi Eyal <adi@burgercom.co.za>
2013-07-19 11:40:05 +0200

    Added dummy admin models for billtracker models

--- code/billtracker/billtracker/admin.py
+++ code/billtracker/billtracker/admin.py
@@ -4,4 +4,11 @@ import models
 class GovInfoScraperAdmin(admin.ModelAdmin):
     pass
 
+class BillStageAdmin(admin.ModelAdmin):
+    pass
+
+class BillAdmin(admin.ModelAdmin):
+    pass
 admin.site.register(models.GovInfoScraper, GovInfoScraperAdmin)
+admin.site.register(models.Bill, BillAdmin)
+admin.site.register(models.BillStage, BillStageAdmin)
