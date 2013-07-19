5b59b969238e8ba92da38f106a6f1be7c8054d91
Adi Eyal <adi@burgercom.co.za>
2013-07-20 00:46:45 +0200

    Added unicode to models

--- code/billtracker/bills/models.py
+++ code/billtracker/bills/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.conf import settings
 from model_utils.managers import InheritanceManager
 
+class BillException(Exception):
+    pass
+
 class Bill(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10)
@@ -10,9 +13,15 @@ class Bill(models.Model):
     def current_stage(self):
         return BillStage.objects.filter(bill=self).order_by("-id").select_subclasses()[0]
 
+    def __unicode__(self):
+        return self.name
+
 class BillStage(models.Model):
     bill = models.ForeignKey(Bill, related_name="stages")
     objects = InheritanceManager()
+
+    def __unicode__(self):
+        return str(self.bill)
 
 class PreparliamentaryStage(BillStage):
     comments_start = models.DateField()
