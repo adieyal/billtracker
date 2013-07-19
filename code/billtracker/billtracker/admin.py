c0065916f6fc9c7311782e8c69ef653cdddbafe6
Adi Eyal <adi@burgercom.co.za>
2013-07-19 23:07:26 +0200

    Improved admin display

--- code/billtracker/billtracker/admin.py
+++ code/billtracker/billtracker/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 import models
 
-class GovInfoScraperAdmin(admin.ModelAdmin):
+class BillAdmin(admin.ModelAdmin):
     pass
 
 class BillStageAdmin(admin.ModelAdmin):
     pass
 
-class BillAdmin(admin.ModelAdmin):
-    pass
+class GovInfoScraperAdmin(admin.ModelAdmin):
+    list_display = ('bill_name', 'bill_code', 'comment_startdate', 'comment_enddate', 'reviewed') 
+    list_filter = ('reviewed',) 
+
 admin.site.register(models.GovInfoScraper, GovInfoScraperAdmin)
 admin.site.register(models.Bill, BillAdmin)
 admin.site.register(models.BillStage, BillStageAdmin)
