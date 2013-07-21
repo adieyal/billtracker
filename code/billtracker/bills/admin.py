51684b3d6abb798ad68cb323c5d21bb47b2fd829
Adi Eyal <adi@burgercom.co.za>
2013-07-22 00:05:28 +0200

    Added admin models for new stages

--- code/billtracker/bills/admin.py
+++ code/billtracker/bills/admin.py
@@ -7,5 +7,13 @@ class BillAdmin(admin.ModelAdmin):
 class BillStageAdmin(admin.ModelAdmin):
     pass
 
+class PreparliamentaryStageAdmin(admin.ModelAdmin):
+    pass
+
+class ParliamentIntroductionAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(models.Bill, BillAdmin)
 admin.site.register(models.BillStage, BillStageAdmin)
+admin.site.register(models.PreparliamentaryStage, PreparliamentaryStageAdmin)
+admin.site.register(models.ParliamentIntroduction, ParliamentIntroductionAdmin)
